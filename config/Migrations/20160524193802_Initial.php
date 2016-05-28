aad988547613185700878b1259ec5c779d64a01d
Masahiko Hyodo <b1011057@fun.ac.jp>
2016-05-28 15:19:15 +0900

    change

--- config/Migrations/20160524193802_Initial.php
+++ config/Migrations/20160524193802_Initial.php
@@ -6,181 +6,30 @@ class Initial extends AbstractMigration
     public function up()
     {
 
-        $this->table('companies')
-            ->addColumn('name', 'string', [
-                'default' => null,
-                'limit' => 255,
-                'null' => false,
-            ])
-            ->addColumn('email', 'string', [
-                'default' => null,
-                'limit' => 255,
-                'null' => false,
-            ])
-            ->addColumn('password', 'string', [
-                'default' => null,
-                'limit' => 255,
-                'null' => false,
-            ])
-            ->addColumn('updated_date', 'timestamp', [
-                'default' => 'CURRENT_TIMESTAMP',
-                'limit' => null,
-                'null' => false,
-            ])
-            ->addColumn('created_date', 'timestamp', [
-                'default' => 'CURRENT_TIMESTAMP',
-                'limit' => null,
-                'null' => false,
-            ])
-            ->create();
-
-        $this->table('company_status')
+        $this->table('mimamon')
             ->addColumn('user_id', 'integer', [
-                'default' => -1,
-                'limit' => 11,
-                'null' => false,
-            ])
-            ->addColumn('company_id', 'integer', [
-                'default' => -1,
-                'limit' => 11,
-                'null' => false,
-            ])
-            ->addColumn('usertag_id', 'integer', [
                 'default' => null,
                 'limit' => 11,
                 'null' => false,
             ])
-            ->addColumn('checked', 'boolean', [
-                'default' => false,
+            ->addColumn('start_time', 'timestamp', [
+                'default' => null,
                 'limit' => null,
                 'null' => false,
             ])
-            ->addColumn('updated_date', 'timestamp', [
-                'default' => 'CURRENT_TIMESTAMP',
-                'limit' => null,
-                'null' => false,
-            ])
-            ->addColumn('created_date', 'timestamp', [
-                'default' => 'CURRENT_TIMESTAMP',
+            ->addColumn('end_time', 'timestamp', [
+                'default' => null,
                 'limit' => null,
                 'null' => false,
             ])
             ->create();
 
-        $this->table('company_tags')
-            ->addColumn('name', 'string', [
-                'default' => null,
-                'limit' => 255,
-                'null' => false,
-            ])
-            ->create();
-
-        $this->table('requests')
-            ->addColumn('user_id', 'integer', [
-                'default' => -1,
-                'limit' => 11,
-                'null' => false,
-            ])
-            ->addColumn('company_id', 'integer', [
-                'default' => -1,
-                'limit' => 11,
-                'null' => false,
-            ])
-            ->addColumn('status', 'integer', [
-                'default' => 0,
-                'limit' => 11,
-                'null' => false,
-            ])
-            ->addColumn('updated_date', 'timestamp', [
-                'default' => 'CURRENT_TIMESTAMP',
-                'limit' => null,
-                'null' => false,
-            ])
-            ->addColumn('created_date', 'timestamp', [
-                'default' => 'CURRENT_TIMESTAMP',
-                'limit' => null,
-                'null' => false,
-            ])
-            ->create();
-
-        $this->table('user_status')
-            ->addColumn('user_id', 'string', [
-                'default' => -1,
-                'limit' => 255,
-                'null' => false,
-            ])
-            ->addColumn('company_id', 'string', [
-                'default' => -1,
-                'limit' => 255,
-                'null' => false,
-            ])
-            ->addColumn('usertag_id', 'string', [
-                'default' => null,
-                'limit' => 255,
-                'null' => false,
-            ])
-            ->addColumn('checked', 'string', [
-                'default' => 0,
-                'limit' => 255,
-                'null' => false,
-            ])
-            ->addColumn('updated_date', 'timestamp', [
-                'default' => 'CURRENT_TIMESTAMP',
-                'limit' => null,
-                'null' => false,
-            ])
-            ->addColumn('created_date', 'timestamp', [
-                'default' => 'CURRENT_TIMESTAMP',
-                'limit' => null,
-                'null' => false,
-            ])
-            ->create();
-
-        $this->table('user_tags')
-            ->addColumn('name', 'string', [
-                'default' => null,
-                'limit' => 255,
-                'null' => false,
-            ])
-            ->create();
-
-        $this->table('users')
-            ->addColumn('name', 'string', [
-                'default' => null,
-                'limit' => 255,
-                'null' => false,
-            ])
-            ->addColumn('email', 'string', [
-                'default' => null,
-                'limit' => 255,
-                'null' => false,
-            ])
-            ->addColumn('password', 'string', [
-                'default' => null,
-                'limit' => 255,
-                'null' => false,
-            ])
-            ->addColumn('updated_date', 'timestamp', [
-                'default' => 'CURRENT_TIMESTAMP',
-                'limit' => null,
-                'null' => false,
-            ])
-            ->addColumn('created_date', 'timestamp', [
-                'default' => 'CURRENT_TIMESTAMP',
-                'limit' => null,
-                'null' => false,
-            ])
-            ->create();
     }
 
     public function down()
     {
-        $this->dropTable('companies');
-        $this->dropTable('company_status');
-        $this->dropTable('company_tags');
-        $this->dropTable('requests');
-        $this->dropTable('user_status');
-        $this->dropTable('user_tags');
-        $this->dropTable('users');
+        $this->dropTable('user_id');
+        $this->dropTable('start_time');
+        $this->dropTable('end_time');
     }
 }
